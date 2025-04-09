f4c4f53cebbd052e92715e4426d8aaf801c502a9
Nazariy Vdovenko <usern@Uristy.kgs.local>
2025-04-09 15:55:47 +0300

    Point5

--- Src/main.c
+++ Src/main.c
@@ -38,12 +38,10 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
-#define SYNC 0xC8;
-#define RC 0x16;
 #define UART_RX_BUF_SIZE 64
-#define PACKET_START_1 0x57
-#define PACKET_START_2 0xAB
-#define PACKET_SIZE	26
+#define WHEEL_CHANNEL 1
+
+
 #define CONTROL_MODE 0x20
 #define CENETER_ANGLE 0x00
 #define ANGULAR_VELOCITY 250
@@ -58,15 +56,9 @@
 /* Private variables ---------------------------------------------------------*/
 
 /* USER CODE BEGIN PV */
-uint16_t counter = 0;
-uint16_t counter1 = 0;
-uint16_t counterrx = 0;
-uint16_t counter1rx = 0;
-uint16_t frame_type;
-
-uint8_t uart_rx_byte;
-uint8_t buffer[UART_RX_BUF_SIZE];
-uint8_t packet[UART_RX_BUF_SIZE];
+uint8_t UART_BYTE_BUFFER;
+uint8_t buffer[64];
+uint8_t packet[64];
 uint8_t packet_pos = 0;
 uint8_t in_packet = 0;  
 
@@ -84,37 +76,39 @@ uint8_t readByteIndex = 0;
 
 uint8_t data_type;
 uint8_t command_type;
-uint8_t DMA_buffer[64];
+
+uint8_t DMA_buffer2[64];
+uint8_t DMA_buffer6[64];
 uint8_t SPI_buffer[64];
-uint8_t RxData_rc[26];
-uint8_t SPIData_rc[26];
-uint16_t rcData[16];
-uint16_t SPIrcData[16];
+uint8_t RxDataUart1[26];
+uint8_t RxDataUart2[26];
+uint8_t RxDataUart6[26];
+uint8_t RxDataSPI[26];
+
+uint16_t RC_HID[16];
+uint16_t RC_TBS[16];
+uint16_t RC_ELRS[16];
+uint16_t RC_SPI[16];
 
 struct CAN_Rx_Message Rx_Msg;
-struct Channels ch;
-
-uint8_t rxData[3];
-
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 /* USER CODE BEGIN PFP */
+
 void unpackChannelData(uint8_t *RxDataRC, uint16_t *RCData);
 void ProcessUARTByte(uint8_t byte);
 void SetAngle(uint16_t angle);
-
-
 void reset_near_control(void);
 void reset_far_control(void);
+
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
 
-struct CAN_Rx_Message
-{
+struct CAN_Rx_Message{
 	int8_t data0;
 	int8_t data1;
 	int8_t data2;
@@ -123,102 +117,83 @@ struct CAN_Rx_Message
 	int8_t data5;
 	int8_t data6;
 	int8_t data7;
-	float angle;
-};
-
-struct Channels
-{
-	int16_t channel1;
-	int16_t channel2;
-	int16_t channel3;
-	int16_t channel4;
-	int16_t channel5;
-	int16_t channel6;
-	int16_t channel7;
-	int16_t channel8;
-};
+	float angle;};
 
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
-	counter++;
     if (huart->Instance == USART1) {
-        ProcessUARTByte(uart_rx_byte);
-        HAL_UART_Receive_IT(&huart1, &uart_rx_byte, 1);
-				reset_far_control();
-    }
+        ProcessUARTByte(UART_BYTE_BUFFER);
+        HAL_UART_Receive_IT(&huart1, &UART_BYTE_BUFFER, 1);}
+		
 		if(huart->Instance == USART2){
-		counterrx++;
 		for(int i = 0; i<26; i++){
-			if(DMA_buffer[i] == 0xC8){
-				data_type = DMA_buffer[i+2];
+			if(DMA_buffer2[i] == 0xC8){
+				data_type = DMA_buffer2[i+2];
 				if(data_type == 0x16){
-				memcpy(RxData_rc, &DMA_buffer[i], 26 * sizeof(RxData_rc[0]));
-					}}}
-		unpackChannelData(RxData_rc, rcData);
-		HAL_UART_Receive_DMA(&huart2, DMA_buffer, sizeof(DMA_buffer));
-		reset_near_control();					
-	}
-}
+				memcpy(RxDataUart2, &DMA_buffer2[i], 26 * sizeof(RxDataUart2[0]));}}}
+		unpackChannelData(RxDataUart2, RC_TBS);
+		HAL_UART_Receive_DMA(&huart2, DMA_buffer2, sizeof(DMA_buffer2));}
+
+    if (huart->Instance == USART6){
+		for(int i = 0; i<26; i++){
+			if(DMA_buffer6[i] == 0xC8){
+				data_type = DMA_buffer6[i+2];
+				if(data_type == 0x16){
+				memcpy(RxDataUart6, &DMA_buffer6[i], 26 * sizeof(RxDataUart6[0]));}}}
+		unpackChannelData(RxDataUart6, RC_ELRS);
+		HAL_UART_Transmit_DMA(&huart6, DMA_buffer6, sizeof(DMA_buffer6));
+		HAL_UART_Receive_DMA(&huart6, DMA_buffer6, sizeof(DMA_buffer6));}}
 
 void HAL_SPI_RxCpltCallback(SPI_HandleTypeDef *hspi) {
     if (hspi == &hspi2) {
 			for(int i = 0; i<26; i++){
 			if(SPI_buffer[i] == '$' && SPI_buffer[i+1] == 'M' && SPI_buffer[i+2] == '>'){}
-				command_type = DMA_buffer[i+3];
+				command_type = SPI_buffer[i+4];
 				if(command_type == MSP_SET_RAW_RC){
-					memcpy(SPIData_rc, &SPI_buffer[i], 26 * sizeof(SPIData_rc[0]));
-					SPIrcData[0] = ((SPIData_rc[1] << 8) | SPIData_rc[0]);
-					SPIrcData[1] = ((SPIData_rc[3] << 8) | SPIData_rc[2]);
-					SPIrcData[2] = ((SPIData_rc[5] << 8) | SPIData_rc[4]);
-					SPIrcData[3] = ((SPIData_rc[7] << 8) | SPIData_rc[6]);
-					SPIrcData[4] = ((SPIData_rc[9] << 8) | SPIData_rc[8]);
-					SPIrcData[5] = ((SPIData_rc[11] << 8) | SPIData_rc[10]);
-					SPIrcData[6] = ((SPIData_rc[13] << 8) | SPIData_rc[12]);
-					SPIrcData[7] = ((SPIData_rc[15] << 8) | SPIData_rc[14]);
+					memcpy(RxDataSPI, &SPI_buffer[i+5], 26 * sizeof(RxDataSPI[0]));
+					RC_SPI[0] = ((RxDataSPI[1] << 8) | RxDataSPI[0]);
+					RC_SPI[1] = ((RxDataSPI[3] << 8) | RxDataSPI[2]);
+					RC_SPI[2] = ((RxDataSPI[5] << 8) | RxDataSPI[3]);
+					RC_SPI[3] = ((RxDataSPI[7] << 8) | RxDataSPI[6]);
+					RC_SPI[4] = ((RxDataSPI[9] << 8) | RxDataSPI[8]);
+					RC_SPI[5] = ((RxDataSPI[11] << 8) | RxDataSPI[10]);
+					RC_SPI[6] = ((RxDataSPI[13] << 8) | RxDataSPI[12]);
+					RC_SPI[7] = ((RxDataSPI[15] << 8) | RxDataSPI[14]);
 				}}}
-		HAL_SPI_Receive_IT(&hspi2, SPI_buffer, sizeof(SPI_buffer));
-}
-
+		HAL_SPI_Receive_IT(&hspi2, SPI_buffer, sizeof(SPI_buffer));}
 
 void ProcessUARTByte(uint8_t byte) {
 	if (!in_packet) {
-		if (byte == PACKET_START_1) {
+		if (byte == 0x57) {
 			packet_pos = 0;
-			buffer[packet_pos++] = byte;
-		}
+			buffer[packet_pos++] = byte;}
 		else{
 		buffer[packet_pos++] = byte;
-		frame_type = buffer[2];
-		if (packet_pos >= PACKET_SIZE && buffer[2] == 0x88 && buffer[0] == PACKET_START_1) {
-				counter1++;
-				memcpy(packet, buffer, PACKET_SIZE);
-				ch.channel1 = (((packet[9] << 8) | packet[8]));
-				ch.channel2 = (((packet[11] << 8) | packet[10]));
-				ch.channel3 = (((packet[13] << 8) | packet[12]));
-				ch.channel4 = (((packet[15] << 8) | packet[14]));
-			  ch.channel5 = (((packet[17] << 8) | packet[16]));
-				ch.channel6 = (((packet[19] << 8) | packet[18]));
-				ch.channel7 = (((packet[21] << 8) | packet[20]));
-				ch.channel8 = (((packet[23] << 8) | packet[22]));
-				in_packet = 0;
-	}}}}
+		if (packet_pos >= 26 && buffer[2] == 0x88){
+				memcpy(RxDataUart1, buffer, sizeof(RxDataUart1));
+				RC_HID[0] = (((RxDataUart1[9] << 8) | RxDataUart1[8]));
+		    RC_HID[1] = (((RxDataUart1[11] << 8) | RxDataUart1[10]));
+				RC_HID[2] = (((RxDataUart1[13] << 8) | RxDataUart1[12]));
+				RC_HID[3] = (((RxDataUart1[15] << 8) | RxDataUart1[14]));
+			  RC_HID[4] = (((RxDataUart1[17] << 8) | RxDataUart1[16]));
+				RC_HID[5] = (((RxDataUart1[19] << 8) | RxDataUart1[18]));
+				RC_HID[6] = (((RxDataUart1[21] << 8) | RxDataUart1[20]));
+				RC_HID[7] = (((RxDataUart1[23] << 8) | RxDataUart1[22]));
+				in_packet = 0;}}}}
 
-void unpackChannelData(uint8_t *RxDataRC, uint16_t *RCData)
-{
+void unpackChannelData(uint8_t *array1, uint16_t *array2){
 	bitsMerged = 0;
 	readValue = 0;
 	readByteIndex = 3;
-
 	for (uint8_t n = 0; n < numOfChannels; n++) {
 		while (bitsMerged < srcBits) {
-			uint8_t readByte = RxDataRC[readByteIndex++];
+			uint8_t readByte = array1[readByteIndex++];
       readValue |= ((uint32_t) readByte) << bitsMerged;
       bitsMerged += 8;
 			}
-			RCData[n] = (readValue & inputChannelMask);
+			array2[n] = (readValue & inputChannelMask);
 			readValue >>= srcBits;
-			bitsMerged -= srcBits;
-	}
-}
+			bitsMerged -= srcBits;}}
+
 void SetAngle(uint16_t angle){
 	uCAN_MSG txMessage;
 	angle_low = (angle + 1024) & 0xFF;
@@ -235,11 +210,10 @@ void SetAngle(uint16_t angle){
 	txMessage.frame.data6 = ANGULAR_VELOCITY;
   txMessage.frame.data7 = txMessage.frame.data0 ^ txMessage.frame.data1 ^ txMessage.frame.data2 
 	^ txMessage.frame.data3 ^ txMessage.frame.data4 ^ txMessage.frame.data5 ^ txMessage.frame.data6;
-  CANSPI_Transmit(&txMessage);
-}
+  CANSPI_Transmit(&txMessage);}
 
-void reset_near_control(void){ch.channel2 = 0;}
-void reset_far_control(void){rcData[1] = 0;}
+//void reset_near_control(void){ch.channel2 = 0;}
+//void reset_far_control(void){rcData[1] = 0;}
 
 /* USER CODE END 0 */
 
@@ -280,13 +254,11 @@ int main(void)
   MX_I2C1_Init();
   MX_USART6_UART_Init();
   /* USER CODE BEGIN 2 */
-	//HAL_GPIO_WritePin(SPI2_CS_GPIO_Port, SPI2_CS_Pin, GPIO_PIN_SET);
-  HAL_UART_Receive_IT(&huart1, &uart_rx_byte, 1);
-	HAL_UART_Receive_DMA(&huart2, DMA_buffer, sizeof(DMA_buffer));
-	
-	
+
+  HAL_UART_Receive_IT(&huart1, &UART_BYTE_BUFFER, sizeof(UART_BYTE_BUFFER));
+	HAL_UART_Receive_DMA(&huart2, DMA_buffer2, sizeof(DMA_buffer2));
+	HAL_UART_Receive_DMA(&huart6, DMA_buffer6, sizeof(DMA_buffer6));
 	HAL_SPI_Receive_IT(&hspi2, SPI_buffer, sizeof(SPI_buffer));	
-	//HAL_SPI_Transmit_IT(&hspi2, txData, 3);
 	
   //uCAN_MSG txMessage;
 	uCAN_MSG rxMessage;
@@ -297,19 +269,14 @@ int main(void)
 	HAL_Delay(1000);
 
   /* USER CODE END 2 */
-
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
   { 
-	if (SPIrcData[1]>0)
-	{angle = (((rcData[1]-172)*0.4395)-360);}
-	if (ch.channel2>0)
-	{angle = ((0.3515 * ch.channel2) - 360);}
-	if (rcData[1]>0)
-	{angle = (((rcData[1]-172)*0.4395)-360);}
+	if (RC_SPI[WHEEL_CHANNEL]>0){angle = (((RC_SPI[WHEEL_CHANNEL]-172)*0.4395)-360);}
+	else if (RC_HID[WHEEL_CHANNEL]>0){angle = ((0.3515 * RC_HID[WHEEL_CHANNEL]) - 360);}
+	else if (RC_TBS[WHEEL_CHANNEL]>0){angle = (((RC_TBS[WHEEL_CHANNEL]-172)*0.4395)-360);}
 	SetAngle(angle);
-
 	
 	if(CANSPI_Receive(&rxMessage)){
 		Rx_Msg.data0 = rxMessage.frame.data0;
@@ -320,17 +287,15 @@ int main(void)
 		Rx_Msg.data5 = rxMessage.frame.data5;
 		Rx_Msg.data6 = rxMessage.frame.data6;
 		Rx_Msg.data7 = rxMessage.frame.data7;
-		Rx_Msg.angle = ((Rx_Msg.data3 << 8) + Rx_Msg.data4) - 1024;
-  }
-	HAL_UART_Receive_DMA(&huart2, DMA_buffer, sizeof(DMA_buffer));
+		Rx_Msg.angle = ((Rx_Msg.data3 << 8) + Rx_Msg.data4) - 1024;}
 	
-	HAL_Delay(50);
-	//HAL_SPI_Transmit_IT(&hspi2, txData, 3);
-	}
+	HAL_UART_Receive_IT(&huart1, &UART_BYTE_BUFFER, sizeof(UART_BYTE_BUFFER));
+	HAL_UART_Receive_DMA(&huart2, DMA_buffer2, sizeof(DMA_buffer2));
+	HAL_UART_Receive_DMA(&huart6, DMA_buffer2, sizeof(DMA_buffer6));
+	HAL_SPI_Receive_IT(&hspi2, SPI_buffer, sizeof(SPI_buffer));	}
+	
     /* USER CODE END WHILE */
-
     /* USER CODE BEGIN 3 */
-	
   /* USER CODE END 3 */
 }
 
